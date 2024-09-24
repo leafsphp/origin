f46dfb1e4e4612ead02c6a6a62c74d4851495252
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-09-24 19:23:40 +0300

    refactor: update routes to use new controller methods and add route protection

--- app/routes/_app.php
+++ app/routes/_app.php
@@ -1,8 +1,13 @@
 <?php
 
-app()->get('/', function () {
-    /**
-     * `render(view, [])` is the same as `echo view(view, [])`
-     */
-    render('index');
-});
+app()->redirect('app', 'app/home');
+app()::group('app', ['namespace'=>'\App\Controllers\App', function() {
+
+    app()->get('home', ['name'=>'app.home', 'HomeController@home']);
+
+    app()->get('profile', ['name'=>'app.profile', 'UserController@profile']);
+
+    app()->post('profile', ['name'=>'app.profile.update', 'UserController@updateProfile']);
+    app()->post('password', ['name'=>'app.password.update', 'UserController@updatePassword']);
+    app()->post('security', ['name'=>'app.security.update', 'UserController@updateSecurity']);  
+}]);
