433ee01b897f1d1b2ff59e5d8d41f547028336cd
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-09-24 19:33:50 +0300

    feat: add error page templates for 400, 403, 404, and 500 errors

---
+++ app/views/errors/403.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Forbiden Access</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+        />
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+        />
+        <style>
+            body {
+                display: flex !important;
+                align-items: center !important;
+                justify-content: center !important;
+                flex-direction: column !important;
+                height: 95vh !important;
+            }
+        </style>
+    </head>
+
+    <body>
+        <h2>
+            Error 4<span role="img" aria-label="O">&#x1f635;</span
+            ><span role="img" aria-label="3">&#x1f635;</span>
+        </h2>
+        <p>Forbiden Access. You don't have permission to access this page.</p>
+    </body>
+</html>
