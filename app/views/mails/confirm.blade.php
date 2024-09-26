a7fc4eda67129226b91f22419d46dcacfef61882
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-09-27 02:29:50 +0300

    refactor: Remove unnecessary comments and whitespace in confirm.blade.php

--- app/views/mails/confirm.blade.php
+++ app/views/mails/confirm.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.mail')
 @section('content')
 
-    <!-- Test Mail -->
     <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="main">
-        <!-- START MAIN CONTENT AREA -->
         <tr>
             <td style="text-align:center; padding-top:1rem;">
                 <img width="200" src="/assets/images/logo-dark.png" alt="Logo">
@@ -27,7 +25,7 @@
                 <p> {{ getenv('app_name') }} </p>
             </td>
         </tr>
-        <!-- END MAIN CONTENT AREA -->
+        
     </table>
 
 @endsection
