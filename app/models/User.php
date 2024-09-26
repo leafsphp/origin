b04de562abbf79c5bf5c6e74f302fc6b626ab879
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-09-27 02:28:49 +0300

    refactor: add two-factor authentication and login notification settings to User model

--- app/models/User.php
+++ app/models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 class User extends Model
 {
     protected $table = "users";
-    protected $fillable = ["username", "fullname", "email", "email_verified", "password", "remember_token", "status", "role", "avatar"];
+    protected $fillable = ["username", "fullname", "email", "email_verified", "password", "remember_token", "status", "role", "avatar", "two_fa", "notify_login"];
     protected $hidden = ["password", "remember_token"];
     protected $with = ["role"];
     public $timestamps = true;
@@ -18,5 +18,11 @@ class User extends Model
     public function role()
     {
         return $this->belongsTo(UserRole::class, "role");
+    }
+
+    # has many user tokens
+    public function tokens()
+    {
+        return $this->hasMany(UserToken::class, "user_id");
     }
 }
